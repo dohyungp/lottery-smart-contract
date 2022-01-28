d7a12e31605c866731b78ce16c347c6512b8e1a9
dohyung <dohyung.prk@gmail.com>
2022-01-28 22:23:34 +0900

    ADD Lottery 테스트 코드를 추가하다

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Lottery {}
+contract Lottery {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getSomeValue() public pure returns (uint256 value) {
+        return 5;
+    }
+}
