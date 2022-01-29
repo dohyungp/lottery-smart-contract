ac9be4a601e7279f7d14732679b288b8389eaad7
dohyung <dohyung.prk@gmail.com>
2022-01-29 22:49:09 +0900

    ADD 베팅기능을 구현하다

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -20,12 +20,40 @@ contract Lottery {
 
     uint256 private _pot;
 
+    event BET(
+        uint256 index,
+        address bettor,
+        uint256 amount,
+        bytes1 challenges,
+        uint256 answerBlockNumber
+    );
+
     constructor() {
         owner = msg.sender;
     }
 
     function getPot() public view returns (uint256) {
         return _pot;
+    }
+
+    /**
+     * @dev 배팅을 한다. 유저는 0.005eth를 보내야 하고, 배팅용 1bytes 글자를 보낸다.
+     * Queue에 저장된 배팅 정보는 distirbute 함수에서 해결된다.
+     * @param challenges 유저가 배팅하는 글자.
+     * @return 함수가 잘 수행되었는지 확인하는 bool값.
+     */
+    function bet(bytes1 challenges) public payable returns (bool) {
+        require(msg.value == BET_AMOUNT, "Not enough ETH");
+        require(pushBet(challenges), "Fail to add new Bet Info");
+
+        emit BET(
+            _tail - 1,
+            msg.sender,
+            msg.value,
+            challenges,
+            block.number + BLOCK_INTERVAL
+        );
+        return true;
     }
 
     function getBetInfo(uint256 index)
