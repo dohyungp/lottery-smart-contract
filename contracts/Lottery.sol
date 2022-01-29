4d1b78ca5f46aabcaeac5d26dff88434ee7f3fe1
dohyung <dohyung.prk@gmail.com>
2022-01-29 14:44:27 +0900

    ADD 베팅 큐를 구현하다

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,14 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 contract Lottery {
     struct BetInfo {
         uint256 answerBlockNumber;
-        address payable better;
-        bytes challenges;
+        address payable bettor;
+        bytes1 challenges;
     }
 
+    uint256 private _tail;
+    uint256 private _head;
+    mapping(uint256 => BetInfo) private _bets;
+
     address public owner;
+
+    uint256 internal constant BLOCK_LIMIT = 256;
+    uint256 internal constant BLOCK_INTERVAL = 3;
+    uint256 internal constant BET_AMOUNT = 5 * 10**15; // 0.005
 
     uint256 private _pot;
 
@@ -18,5 +26,37 @@ contract Lottery {
 
     function getPot() public view returns (uint256) {
         return _pot;
+    }
+
+    function getBetInfo(uint256 index)
+        public
+        view
+        returns (
+            uint256 answerBlockNumber,
+            address bettor,
+            bytes1 challenges
+        )
+    {
+        BetInfo memory b = _bets[index];
+        answerBlockNumber = b.answerBlockNumber;
+        bettor = b.bettor;
+        challenges = b.challenges;
+    }
+
+    function pushBet(bytes1 challenges) public returns (bool) {
+        BetInfo memory b;
+        b.bettor = payable(msg.sender);
+        b.answerBlockNumber = block.number + BLOCK_INTERVAL;
+        b.challenges = challenges;
+
+        _bets[_tail] = b;
+        _tail++;
+
+        return true;
+    }
+
+    function popBet(uint256 index) public returns (bool) {
+        delete _bets[index];
+        return true;
     }
 }
